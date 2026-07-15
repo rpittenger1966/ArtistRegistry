1b86effbb9686437d1d6b4ad723bbcc2eb006c03
Bob <pittenger@pointstar.com>
2026-07-15 06:50:59 -0400

    Added artist pages and test project

---
+++ ArtistRegistry.WebScaper.Tests/HtmlParsingTests.cs
@@ -0,0 +1,94 @@
+﻿using ArtistEntryRegistry.Standard.Data.Providers;
+using ArtistRegistry.Core.WebScraperLibrary;
+using ArtistRegistry.Standard.Common;
+using ArtistRegistry.Standard.Data;
+using ArtistRegistry.Standard.Data.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtistRegistry.WebScaper.Tests
+{
+	[TestClass]
+	public class HtmlParsingTests
+	{
+		public string LoadContentFile(string fileName)
+		{
+			return File.ReadAllText(fileName);
+		}
+
+		[TestMethod]
+		public void TestParseContentDiv()
+		{
+			string source = LoadContentFile(@"M:\Visual Studio Solutions\ArtistRegistry\OhioArtistryPages\Index\A-2.html");
+
+			int count = 0;
+			int startIndex = 0;
+			while (true)
+			{
+				ParseResult result = HtmlParser.GetCnCircledContentDiv(source, startIndex);
+				if (result.HasValue == false) break;
+
+				startIndex = result.EndIndex + 1;
+
+				Console.WriteLine("=================================");
+				Console.WriteLine(result.Content);
+				Console.WriteLine("");
+				++count;
+			}
+
+			Console.WriteLine($"Found {count} entries");
+
+		}
+
+		[TestMethod]
+		public void TestParseContentFields()
+		{
+			string source = File.ReadAllText(@"M:\Visual Studio Solutions\ArtistRegistry\ArtistRegistry.WebScaper.Tests\SampleEntry.html");
+
+			string slug;
+			slug = HtmlParser.ParseSlug(source);
+			Console.WriteLine($"Slug={slug}");
+
+
+			string htmlTag = "span";
+			string cssClass = "given-name";
+			string value = HtmlParser.ParseCircledContentSpanHtml(source, htmlTag, cssClass);
+			Console.WriteLine($"{cssClass}={value}");
+
+			//cssClass = "family-name";
+			//value = HtmlParser.ParseCircledContentSpanHtml(source, htmlTag, cssClass);
+			//Console.WriteLine($"{cssClass}={value}");
+
+			htmlTag = "div";
+			cssClass = "cn-excerpt";
+			value = HtmlParser.ParseCircledContentSpanHtml(source, htmlTag, cssClass);
+			Console.WriteLine($"{cssClass}={value}");
+
+		}
+
+		[TestMethod]
+		public async Task TestParseAndUpsertIndexFile()
+		{
+			await OhioArtistRegistryImportService.ImportIndexPage("D", 1);
+
+		}
+
+		[TestMethod]
+		public async Task TestParseAndUpsertFirstLetter()
+		{
+			await OhioArtistRegistryImportService.ImportFirstLetter("F");
+
+		}
+
+		[TestMethod]
+		public async Task TestParseAndUpsertAll()
+		{
+			await OhioArtistRegistryImportService.ImportAll();
+
+		}
+
+	}  // end of class
+}  // end of namespace
---
+++ ArtistRegistry.WebScaper.Tests/WebScraperTests.cs
@@ -0,0 +1,122 @@
+﻿using ArtistRegistry.Core.WebScraperLibrary;
+using ArtistRegistry.Standard.Common;
+using ArtistRegistry.Standard.Data;
+using FirstLetterRegistry.Standard.Data.Providers;
+
+namespace ArtistRegistry.WebScaper.Tests
+{
+	[TestClass]
+	public sealed class WebScraperTests
+	{
+
+		[TestMethod]
+		public async Task TestUpdateFirstLetterData()
+		{
+			FirstLetterProvider provider = new FirstLetterProvider(ConnectionStringFactory.GetConnectionString(), Guid.Empty);
+
+			List<FirstLetter> list = await provider.GetFirstLettersAsync();
+			Console.WriteLine($"There are {list.Count} items");
+
+			FirstLetter a = list[0];
+			a.PageCount = 1;
+			await provider.UpdateFirstLetterAsync(a);
+		}
+
+
+		[TestMethod]
+		public async Task ScapeFirstLetterPages()
+		{
+			FirstLetterProvider provider = new FirstLetterProvider(ConnectionStringFactory.GetConnectionString(), Guid.Empty);
+
+			List<FirstLetter> list = await provider.GetFirstLettersAsync();
+			Console.WriteLine($"There are {list.Count} items");
+
+			string outputFolder = @"M:\Visual Studio Solutions\ArtistRegistry\OhioArtistryPages\FirstLetter";
+
+
+			foreach (var firstLetter in list)
+			{
+				if (firstLetter.FirstLetterId == "A") continue;
+				if (firstLetter.FirstLetterId == "B") continue;
+				if (firstLetter.FirstLetterId == "C") continue;
+				if (firstLetter.FirstLetterId == "D") continue;
+				if (firstLetter.FirstLetterId == "E") continue;
+				if (firstLetter.FirstLetterId == "F") continue;
+				if (firstLetter.FirstLetterId == "G") continue;
+				if (firstLetter.FirstLetterId == "H") continue;
+				if (firstLetter.FirstLetterId == "I") continue;
+
+
+				Console.WriteLine($"Processing {firstLetter.FirstLetterId}");
+				try
+				{
+					OhioArtistScaperService service = new OhioArtistScaperService();
+					service.DownloadContentForFirstLetter(firstLetter, outputFolder);
+
+					await provider.UpdateFirstLetterAsync(firstLetter);
+
+					System.Threading.Thread.Sleep(1000 * 10);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("**********************");
+					Console.WriteLine(ex.Message);
+					return;
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TestParseFirstLetterPageCount()
+		{
+			char character = 'e';
+			string outputFolder = @"M:\Visual Studio Solutions\ArtistRegistry\OhioArtistryPages\FirstLetter";
+			string fileName = Path.Combine(outputFolder, "e.html");
+			int count = OhioArtistScaperService.CountPages(character, fileName);
+			Console.WriteLine($"Count={count}");
+		}
+
+		[TestMethod]
+		public async Task ScapeIndexPages()
+		{
+			FirstLetterProvider provider = new FirstLetterProvider(ConnectionStringFactory.GetConnectionString(), Guid.Empty);
+
+			List<FirstLetter> list = await provider.GetFirstLettersAsync();
+			Console.WriteLine($"There are {list.Count} items");
+
+			string outputFolder = @"M:\Visual Studio Solutions\ArtistRegistry\OhioArtistryPages\Index";
+
+
+			foreach (var firstLetter in list)
+			{
+				if (firstLetter.FirstLetterId == "A") continue;
+				if (firstLetter.FirstLetterId == "B") continue;
+				if (firstLetter.FirstLetterId == "C") continue;
+				if (firstLetter.FirstLetterId == "D") continue;
+				if (firstLetter.FirstLetterId == "E") continue;
+//				if (firstLetter.FirstLetterId == "F") return;
+				//if (firstLetter.FirstLetterId == "G") continue;
+				//if (firstLetter.FirstLetterId == "H") continue;
+				//if (firstLetter.FirstLetterId == "I") continue;
+
+
+				Console.WriteLine($"Processing {firstLetter.FirstLetterId}");
+				try
+				{
+					OhioArtistScaperService.DownloadIndexPages(firstLetter, outputFolder);
+
+					System.Threading.Thread.Sleep(1000 * 20);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("**********************");
+					Console.WriteLine(ex.Message);
+					return;
+				}
+			}
+		}
+
+
+
+	}  // end of class
+}  // end of namespace
