242b6d62a5a86d9042bb038a13ea7170c8675540
Bob <pittenger@pointstar.com>
2026-07-15 14:52:45 -0400

    Added missing files

---
+++ ArtistRegistry.Standard.Common/ConnectionStringFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArtistRegistry.Standard.Common
+{
+	public class ConnectionStringFactory
+	{
+		static public string GetConnectionString()
+		{
+			return "Server=(local);Database=ArtistRegistry;Trusted_Connection=True;TrustServerCertificate=True;";
+		}
+	}
+}
